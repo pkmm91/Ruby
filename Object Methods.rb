ed7b8caabef1ec08f18850879d1176e5763064b7
PANKAJ KUMAR <pknit1291@gmail.com>
2015-08-10 21:07:17 +0530

    Create Object Methods.rb

---
+++ Object Methods.rb
@@ -0,0 +1,7 @@
+
+
+author : Pankaj Kumar
+
+Each object in Ruby may have methods associated with it. To demonstrate this, we want you to print whether a number is even or odd. A number has an even? method associated with it, which returns true or false based on the parity of the number.
+
+print number.even?
